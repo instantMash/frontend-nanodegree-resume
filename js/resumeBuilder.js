0df714d8e80e13a7c96827f1f0f9156725c6135a
Greg Davenport <gregdavenport@Gregs-MacBook-Pro.local>
2016-12-04 10:15:46 -0800

    Add JS objects for each resume section.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,166 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+// Create objects for each resume section: Work, Projects, Bio, and Education.
+
+var work = {
+  "jobs" : [
+    {
+      "employer" : "Corwin Beverage",
+      "title" : "Senior Graphic Designer",
+      "location" : "Ridgefield, WA",
+      "dates" : "2013 - present",
+      "description" : "Creative lead on internal design team."
+    },
+    {
+      "employer" : "Formations Design Group",
+      "title" : "Graphic Designer",
+      "location" : "Vancouver, WA",
+      "dates" : "2007 - 2012",
+      "description" : "Graphic design and web development in small, versatile design agency."
+    },
+    {
+      "employer" : "Premier Graphics",
+      "title" : "Prepress Technician",
+      "location" : "Bellingham, WA",
+      "dates" : "2001 - 2007",
+      "description" : "Prepare design files for accurate print output. Preflight, proofing, and last-minute edits."
+    }
+  ]
+};
+
+var projects = {
+  "projects" : [
+    {
+      "title" : "Backwoods Brewery",
+      "dates" : "2014 - 2016",
+      "description" : "Package design & branding",
+      "images" : [
+        "images/backwoods-thumbnail.jpg"
+      ]
+    },
+    {
+      "title" : "Terminal Gravity",
+      "dates" : "2016",
+      "description" : "Package design",
+      "images" : [
+        "images/terminal-gravity-thumb.jpg"
+      ]
+    },
+    {
+      "title" : "Kendall's Pioneer Distributing",
+      "dates" : "2014",
+      "description" : "Branding & logo design",
+      "images" : [
+        "images/kendalls-thumb.jpg"
+      ]
+    }
+  ]
+};
+
+var bio = {
+  "name" : "Greg Davenport",
+  "role" : "Graphic Designer",
+  "welcomeMesssage" : "Let's make cool stuff together!",
+  "biopic" : "images/fry.jpg",
+  "contacts" : {
+    "mobile" : "360-869-6933",
+    "email" : "gregdavenport13@gmail.com",
+    "github" : "instantMash",
+    "twitter" : " ",
+    "location" : "Ridgefield, WA"
+  },
+  "skills" : [
+    "Graphic design",
+    "Print production",
+    "Front-end web development",
+    "Adobe Creative Suite"
+  ]
+};
+
+var education = {
+  "schools" : [
+    {
+      "name" : "Gordon College",
+      "location" : "Wenham, MA",
+      "degree" : "BA",
+      "dates" : "1995 - 2000",
+      "url" : " ",
+      "majors" : [
+        "Fine Art/Painting"
+      ]
+    },
+    {
+      "name" : "Whatcom Community College",
+      "location" : "Bellinghame, WA",
+      "degree" : "AA",
+      "dates" : "2004 - 2006",
+      "url" : " ",
+      "major" : [
+        "Graphic Design"
+      ]
+    }
+  ],
+  "onlineCourses" : [
+    {
+      "title" : "Front-end Nanodegree",
+      "school" : "Udacity",
+      "dates" : "2016 - 2017",
+      "url" : "udacity.com"
+    }
+  ]
+};
+
+// Create the display functions for each object. Functions are encapsulated within their respective objects using dot notation.
+
+work.display = function(){
+
+};
+
+projects.display = function(){
+
+};
+
+bio.display = function(){
+
+};
+
+education.display = function(){
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
